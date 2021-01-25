e7909e4fea4db3ee52d0cd5e13a1266075fc760c
sushmitha2198 <43083344+sushmitha2198@users.noreply.github.com>
2021-01-25 17:52:40 +0530

    Update done by sushmitha.k.2198@gmail.com.

--- helloworld/src/main/java/com/mindtree/helloworld/HelloWorldApplication.java
+++ helloworld/src/main/java/com/mindtree/helloworld/HelloWorldApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HelloWorldApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(HelloWorldApplication.class, args);
+		SpringApplication.run(HelloWorldApplication.class,args);
 	}
 
 }
